c41137f86f806864a080bc7acc9cf8bb3def04a6
Ginger Marie <gingermarie@gingers-Air.lan>
2023-05-09 07:53:39 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import { HashRouter} from 'react-router-dom';
 import Carousel from './Carousel/Carousel.js'
 
 
 function App() {
 
   return (
-    <div>
-      <Carousel />
-    </div>
+    <HashRouter>
+      <div>
+        <Carousel />
+      </div>
+    </HashRouter>
   )
 }
 
